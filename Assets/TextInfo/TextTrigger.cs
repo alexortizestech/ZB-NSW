ac0355483e661b49bcd5fe5cfdcaef86ecfedf6b
Alejandro Ortiz <alejandro.ortiz@escuelaestech.es>
2023-03-08 13:24:26 +0100

    commit

--- Assets/TextInfo/TextTrigger.cs
+++ Assets/TextInfo/TextTrigger.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Rewired;
 public class TextTrigger : MonoBehaviour
 {
+    public Player player;
     bool IsTriggered;
     public Dialogue dialogue;
     public GameObject globitoText;
+    public int playerID = 0;
+
+    private void Start()
+    {
+        player = ReInput.players.GetPlayer(playerID);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player") && !IsTriggered)
         {
-            globitoText.SetActive(true);
-            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-            IsTriggered = true;
-            Destroy(this.gameObject);
+            Debug.Log("EnteredDialogue");
+            if (player.GetButtonDown("Teleport"))
+            {
+                Debug.Log("TriggeredDialogue");
+                globitoText.SetActive(true);
+                FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+                IsTriggered = true;
+                Destroy(this.gameObject);
+            }
+          
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && IsTriggered)
+        {
+            IsTriggered = false;
         }
     }
 
